05886f1586aa7b508eb075f43e22177e2f11ac79
Anantd2610 <anantdeore2610@gmail.com>
2021-08-09 17:12:41 +0530

    added date format

--- Spring-learn/src/main/java/com/fis/Springlearn/SpringLearnApplication.java
+++ Spring-learn/src/main/java/com/fis/Springlearn/SpringLearnApplication.java
@@ -1,13 +1,32 @@
 package com.fis.Springlearn;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SpringLearnApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws java.text.ParseException {
 		SpringApplication.run(SpringLearnApplication.class, args);
+		displayDate();
+	}
+
+	static void displayDate() {
+		ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
+		SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
+		Date parseDate = null;
+		try {
+			parseDate = format.parse("31/12/2018");
+		} catch (java.text.ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(parseDate);
 	}
 
 }
