6b8a30a45e2f060db358ba15c2b7360cd9f77caa
Anantd2610 <anantdeore2610@gmail.com>
2021-08-09 17:59:49 +0530

    implemented incorporate logging

--- spring-learn/src/main/java/com/fis/springlearn/SpringLearnApplication.java
+++ spring-learn/src/main/java/com/fis/springlearn/SpringLearnApplication.java
@@ -1,7 +1,9 @@
-package com.fis.Springlearn;
+package com.fis.springlearn;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,22 +13,28 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 @SpringBootApplication
 public class SpringLearnApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
+
 	public static void main(String[] args) throws java.text.ParseException {
 		SpringApplication.run(SpringLearnApplication.class, args);
 		displayDate();
 	}
 
 	static void displayDate() {
+		LOGGER.info("START");
 		ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
 		SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
 		Date parseDate = null;
 		try {
 			parseDate = format.parse("31/12/2018");
+//			System.out.println(parseDate);
+			LOGGER.debug(parseDate.toString());
 		} catch (java.text.ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println(parseDate);
+
+		LOGGER.info("END");
 	}
 
 }
